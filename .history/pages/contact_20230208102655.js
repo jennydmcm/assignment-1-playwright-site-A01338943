6d498cf27132772b95da6c1e0792048e9793156b
jennydmcm <jenniferdmcmahon@gmail.com>
2023-02-08 14:11:44 -0800

    styles added

---
+++ .history/pages/contact_20230208102655.js
@@ -0,0 +1,55 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Contact.module.css";
+import Bg from "public/background-images/backgroundMountainUpsideDown.jpg";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>{"Contact Us"}</title>
+        <meta name="author" content={"MDIA 2109"} />
+        <meta property="og:title" content={"Assignment #1 - Contact Us Page"} />
+        <meta
+          property="og:description"
+          content={"BCIT Digital Design and Development Diploma"}
+        />
+        <link rel="icon" href={"/favicon.png"} />
+      </Head>
+      <main className={styles.main}>
+        <div>
+          <hr></hr>
+          <h1 className={styles.header}>Contact Us</h1>
+          <hr></hr>
+          <p className={styles.subhead}>Want to discuss? Lets chat!</p>
+          <Image
+            src={Bg}
+            fill
+            style={{
+              zIndex: -1,
+            }}
+          ></Image>
+        </div>
+      </main>
+      <form>
+        <fieldset>
+          <table>
+            <thead>
+              <tr></tr>
+              <tr></tr>
+            </thead>
+            <thead></thead>
+          </table>
+        </fieldset>
+      </form>
+      <a>
+        <span>
+          <img src="/backgroundMountain.jpg"></img>
+        </span>
+      </a>
+    </>
+  );
+}
